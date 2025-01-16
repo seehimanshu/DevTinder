7b37c992740f403988ccfd45397c197ab801e76f
seehimanshu <himanshu.thakur2812@gmail.com>
2025-01-16 18:32:54 +0530

    learned advance rounting

--- src/app.js
+++ src/app.js
@@ -2,21 +2,36 @@ const express = require("express");
 
 const app = express(); // creating an instance of express
 
+
+// This will only handle GET call to /user
+app.get("/user" ,(req,res)=>{
+    res.send({firstname: "Himanshu ",lastname:"Sengar"});
+});
+
+app.get("/ab?c" ,(req,res)=>{// ab?c----> conditional rounting
+    res.send({firstname: "Himanshu ",lastname:""});
+});
+app.get("/ab+c" ,(req,res)=>{ // ab+c -----> we can use like this abbbbbbbbbbc we can but as many as b in b/w
+    res.send({firstname: "Himanshu ",lastname:"Sengar"});
+});
+app.get("/ab*cd" ,(req,res)=>{ // ab*cd ------> we can put anything b/w ab and cd abfdgfdghfhcd *  means we can put anything in place of it
+    res.send({firstname: "Himanshu ",lastname:"Sengar"});
+});
+app.get("/a(bc)?d" ,(req,res)=>{ // now here bc is optional
+    res.send({firstname: "Himanshu ",lastname:"Sengar"});
+});
+app.post("/user" ,(req,res)=>{
+    res.send("Data saved successfully ");
+})
+app.delete("/user" ,(req,res)=>{
+    res.send("Data delete successfully ");
+})
+
+// this route will match all the http method API calls to/test
 app.use("/test",(req,res)=>{
     res.send("Hello from the server");
 })// request handler func
-app.use("/hello",(req,res)=>{
-    res.send("Hello hello heloo");
-})
-app.use("/bye",(req,res)=>{
-    res.send("Bye bye bye");
-})
-app.use("/chai",(req,res)=>{
-    res.send("chai bye chai");
-})
-app.use("/",(req,res)=>{
-    res.send("Himanshu Sengar");
-})
+
 app.listen(3000 , ()=>{
     console.log("server is successfuly listen on port 3000");
 });
