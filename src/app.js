8dc99d546a7bd564079f958c89de291e7f29d3b9
seehimanshu <himanshu.thakur2812@gmail.com>
2025-01-17 13:02:31 +0530

    writed middleware auth

--- src/app.js
+++ src/app.js
@@ -1,57 +1,53 @@
-const express = require("express");
+const express =require("express");
 
-const app = express(); // creating an instance of express
+const app=express();
+// if we use this way then we have write the authentication logic in every 
+// route handler so intead of this we will use the app.use to do this work
+// ---------->>>>>>><<<<<<<<-------------
 
+// app.get("/admin/getAllData",(req,res)=>{
+//     const token="xyz";
+//     const isAdminAuthorized = token ==='xyz';
 
-// This will only handle GET call to /user
-// app.get("/user" ,(req,res)=>{
-//     res.send({firstname: "Himanshu ",lastname:"Sengar"});
+//     if(isAdminAuthorized){
+//         res.send("All Data sent");
+//     }
+//     else{
+//         res.status(401).send("Unauthorized request");
+//     }
 // });
 
-// app.get("/ab?c" ,(req,res)=>{// ab?c----> conditional rounting
-//     res.send({firstname: "Himanshu ",lastname:""});
-// });
-// app.get("/ab+c" ,(req,res)=>{ // ab+c -----> we can use like this abbbbbbbbbbc we can but as many as b in b/w
-//     res.send({firstname: "Himanshu ",lastname:"Sengar"});
-// });
-// app.get("/ab*cd" ,(req,res)=>{ // ab*cd ------> we can put anything b/w ab and cd abfdgfdghfhcd *  means we can put anything in place of it
-//     res.send({firstname: "Himanshu ",lastname:"Sengar"});
-// });
-// app.get("/a(bc)?d" ,(req,res)=>{ // now here bc is optional
-//     res.send({firstname: "Himanshu ",lastname:"Sengar"});
-// });
-// app.post("/user" ,(req,res)=>{
-//     console.log(req.query);// this will give user request param
-//     res.send("Data saved successfully ");
-// })
-// app.get("/user/:userId" ,(req,res)=>{ // this will give user id from the url
-//     console.log(req.params)
-//     res.send({firstname: "Himanshu ",lastname:"Sengar"});
-// });
-// app.get("/user/:userId/:name/:password" ,(req,res)=>{ // this is dynamic param
-//     console.log(req.params);
-//     res.send({firstname: "Himanshu ",lastname:"Sengar"});
-// });
-// app.delete("/user" ,(req,res)=>{
-//     res.send("Data delete successfully ");
+// app.get("/admin/DeleteUser",(req,res)=>{
+//     res.send("User deleted");
 // })
 
+// -------------->>>>>>>><<<<<<<<<<<<<<<------------
 
-// this how to pass two request handler
-app.use("/test",(req,res,next)=>{
-    // res.send("1st Response");
-    console.log("Handling the route user 1!!")
-    next();
-} ,(req,res)=>{
-    res.send("2nd Response");
-    console.log("Handling the route user 2!!")
+app.use("/admin",(req,res,next)=>{
+    const token="xz";
+    const isAdminAuthorized = token ==='xyz';
+
+    if(!isAdminAuthorized){
+        res.status(401).send("Unauthorized request");
+    }
+    else{
+        next();
+    }
 })
 
-// this route will match all the http method API calls to/test
-// app.use("/test",(req,res)=>{
-//     res.send("Hello from the server");
-// })// request handler func
+app.get("/admin/getAllData",(req,res)=>{
+    console.log("data handler");
+    res.send("All Data sent");
+})
+app.get("/admin/DeleteUser",(req,res)=>{
+        res.send("User deleted");
+    })
 
-app.listen(3000 , ()=>{
-    console.log("server is successfuly listen on port 3000");
-});
+    // now if we can write other route that didn't required this authentication
+
+app.get("/user",(req,res)=>{
+    res.send("User details")
+})
+app.listen(3000,()=>{
+    console.log("Server is succesfully Listening on port 3000")
+}) 
