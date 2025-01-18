4f6324f8749365bf602b2a9cc44e419e1c6da477
seehimanshu <himanshu.thakur2812@gmail.com>
2025-01-18 18:14:38 +0530

    build our db and pushed the data dynamically

--- src/app.js
+++ src/app.js
@@ -1,53 +1,38 @@
 const express =require("express");
-
+const {connectDB} = require("./config/database");
 const app=express();
-// if we use this way then we have write the authentication logic in every 
-// route handler so intead of this we will use the app.use to do this work
-// ---------->>>>>>><<<<<<<<-------------
+const {User} = require("./models/user");
 
-// app.get("/admin/getAllData",(req,res)=>{
-//     const token="xyz";
-//     const isAdminAuthorized = token ==='xyz';
+app.use(express.json());
 
-//     if(isAdminAuthorized){
-//         res.send("All Data sent");
-//     }
-//     else{
-//         res.status(401).send("Unauthorized request");
-//     }
-// });
+app.post("/signup", async(req,res)=>{
 
-// app.get("/admin/DeleteUser",(req,res)=>{
-//     res.send("User deleted");
-// })
+    //Creating a new instance of the user model-----> in short creating a new user
+    const user = new User(req.body);
 
-// -------------->>>>>>>><<<<<<<<<<<<<<<------------
-
-app.use("/admin",(req,res,next)=>{
-    const token="xz";
-    const isAdminAuthorized = token ==='xyz';
-
-    if(!isAdminAuthorized){
-        res.status(401).send("Unauthorized request");
+    try{
+        await user.save();
+        res.send("User Added Successfully...")
     }
-    else{
-        next();
+    catch(err){
+        res.status(400).send("Error saving the user:" + err.message);
     }
+    
 })
-
-app.get("/admin/getAllData",(req,res)=>{
-    console.log("data handler");
-    res.send("All Data sent");
-})
-app.get("/admin/DeleteUser",(req,res)=>{
-        res.send("User deleted");
-    })
-
-    // now if we can write other route that didn't required this authentication
 
 app.get("/user",(req,res)=>{
-    res.send("User details")
+    res.send("user is here")
 })
-app.listen(3000,()=>{
-    console.log("Server is succesfully Listening on port 3000")
-}) 
+
+
+connectDB()
+    .then(()=>{
+        console.log("Database connection establised...");
+        app.listen(3000,()=>{
+            console.log("Server is successfully listening on port 3000...");
+        });
+    })
+    .catch((err)=>{
+        console.log("Database connection cannot be establised...");
+    })
+
---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const connectDB = async() =>{
+    await mongoose.connect(
+        "mongodb+srv://sengarhimanshu583:Kaxsyb17Mests2w5@cluster0.kd9dn.mongodb.net/DevTinder"
+    );
+};
+
+module.exports={
+    connectDB
+}
