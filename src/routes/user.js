a20e205d32caaecde21704b268d0fc58056d7a00
seehimanshu <himanshu.thakur2812@gmail.com>
2025-02-19 12:36:04 +0530

    added user connections api

--- src/routes/user.js
+++ src/routes/user.js
@@ -27,5 +27,21 @@ userRouter.get("/user/requests/received", userAuth , async(req,res)=>{
 
 })
 
+userRouter.get("/user/connections", userAuth, async(req,res)=>{
+    const loggedInUser = req.user;
 
+    const connectionRequests = await ConnectionRequest.find({
+        $or:[
+            {fromUserId:loggedInUser._id, status:"accepted"},
+            {toUserId:loggedInUser._id, status:"accepted"}
+        ]
+    }).populate("fromUserId",USER_SAVE_DATA).populate("toUserId",USER_SAVE_DATA)
+
+    const data = connectionRequests.map((row)=>{
+        if(row.fromUserId.toString() === loggedInUser._id.toString() ){
+            return row.toUserId;
+        }
+        return row.fromUserId;
+    })
+})
 module.exports = userRouter;
